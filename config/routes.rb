138ce4462ff8bcdb77aa16e86b55e19f6ebb30fb
Yige Yang <15700783441@163.com>
2020-11-30 17:58:39 +0000

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-
+  #Home page is the root
   root 'events#index', as:'home'
 
+  #Resource paths
   resources :events do
     resources :comments
     resources :guests
     patch 'booking', on: :member
   end
 
+  #Contact page
   get 'contact', to:'events#contact'
   post 'request_contact', to:'events#request_contact'
 end
